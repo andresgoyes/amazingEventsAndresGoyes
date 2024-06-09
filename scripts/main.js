aa9e072584fd2f7cb283c603605ae73ce5584287
andresgoyes <andres.goyes@correounivalle.edu.co>
2024-06-08 22:31:15 -0500

    Avance del Sprint 4 en Nueva Rama (Sprint4)

--- scripts/main.js
+++ scripts/main.js
@@ -1,260 +1,76 @@
-const data = {
-    currentDate: "2023-01-01",
-    events: [
-        {
-            _id: "639c723b992482e5f2834be9",
-            name: "Collectivities Party",
-            image: "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
-            date: "2022-12-12",
-            description:
-                "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-            category: "Food Fair",
-            place: "Room A",
-            capacity: 45000,
-            assistance: 42756,
-            price: 5,
-            __v: 0,
-        },
-        {
-            _id: "639c723b992482e5f2834beb",
-            name: "Korean style",
-            image: "https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
-            date: "2023-08-12",
-            description:
-                "Enjoy the best Korean dishes, with international chefs and awesome events.",
-            category: "Food Fair",
-            place: "Room A",
-            capacity: 45000,
-            price: 10,
-            __v: 0,
-            estimate: 42756,
-        },
-        {
-            _id: "639c723c992482e5f2834bed",
-            name: "Jurassic Park",
-            image: "https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
-            date: "2022-11-02",
-            description:
-                "Let's go meet the biggest dinosaurs in the paleontology museum.",
-            category: "Museum",
-            place: "Field",
-            capacity: 82000,
-            price: 15,
-            __v: 0,
-            assistance: 65892,
-        },
-        {
-            _id: "639c723c992482e5f2834bef",
-            name: "Parisian Museum",
-            image: "https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
-            date: "2023-11-02",
-            description:
-                "A unique tour in the city of lights, get to know one of the most iconic places.",
-            category: "Museum",
-            place: "Paris",
-            capacity: 8200,
-            estimate: 8200,
-            price: 3500,
-            __v: 0,
-        },
-        {
-            _id: "639c723c992482e5f2834bf1",
-            name: "Comicon",
-            image: "https://i.postimg.cc/KYD0jMf2/comicon.jpg",
-            date: "2022-02-12",
-            description:
-                "For comic lovers, all your favourite characters gathered in one place.",
-            category: "Costume Party",
-            place: "Room C",
-            capacity: 120000,
-            assistance: 110000,
-            price: 54,
-            __v: 0,
-        },
-        {
-            _id: "639c723c992482e5f2834bf3",
-            name: "Halloween Night",
-            image: "https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
-            date: "2023-02-12",
-            description: "Come with your scariest costume and win incredible prizes.",
-            category: "Costume Party",
-            place: "Room C",
-            capacity: 12000,
-            estimate: 9000,
-            price: 12,
-            __v: 0,
-        },
-        {
-            _id: "639c723c992482e5f2834bf5",
-            name: "Metallica in concert",
-            image: "https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
-            date: "2023-01-22",
-            description: "The only concert of the most emblematic band in the world.",
-            category: "Music Concert",
-            place: "Room A",
-            capacity: 138000,
-            estimate: 138000,
-            price: 150,
-            __v: 0,
-        },
-        {
-            _id: "639c723c992482e5f2834bf7",
-            name: "Electronic Fest",
-            image: "https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
-            date: "2022-01-22",
-            description:
-                "The best national and international DJs gathered in one place.",
-            category: "Music Concert",
-            place: "Room A",
-            capacity: 138000,
-            assistance: 110300,
-            price: 250,
-            __v: 0,
-        },
-        {
-            _id: "639c723d992482e5f2834bf9",
-            name: "10K for life",
-            image: "https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
-            date: "2022-03-01",
-            description: "Come and exercise, improve your health and lifestyle.",
-            category: "Race",
-            place: "Soccer field",
-            capacity: 30000,
-            assistance: 25698,
-            price: 3,
-            __v: 0,
-        },
-        {
-            _id: "639c723d992482e5f2834bfb",
-            name: "15K NY",
-            image: "https://i.postimg.cc/zv67r65z/15kny.jpg",
-            date: "2023-03-01",
-            description:
-                "We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-            category: "Race",
-            place: "New York",
-            capacity: 3000000,
-            price: 3,
-            __v: 0,
-            estimate: 2569800,
-        },
-        {
-            _id: "639c723d992482e5f2834bfd",
-            name: "School's book fair",
-            image: "https://i.postimg.cc/Sst763n6/book1.jpg",
-            date: "2023-10-15",
-            description: "Bring your unused school book and take the one you need.",
-            category: "Book Exchange",
-            place: "Room D1",
-            capacity: 150000,
-            estimate: 123286,
-            price: 1,
-            __v: 0,
-        },
-        {
-            _id: "639c723d992482e5f2834bff",
-            name: "Just for your kitchen",
-            image: "https://i.postimg.cc/05FhxHVK/book4.jpg",
-            date: "2022-11-09",
-            description:
-                "If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-            category: "Book Exchange",
-            place: "Room D6",
-            capacity: 130000,
-            assistance: 90000,
-            price: 100,
-            __v: 0,
-        },
-        {
-            _id: "639c723d992482e5f2834c01",
-            name: "Batman",
-            image: "https://i.postimg.cc/vH52y81C/cinema4.jpg",
-            date: "2022-3-11",
-            description: "Come see Batman fight crime in Gotham City.",
-            category: "Cinema",
-            place: "Room D1",
-            capacity: 11000,
-            assistance: 9300,
-            price: 225,
-            __v: 0,
-        },
-        {
-            _id: "639c723d992482e5f2834c03",
-            name: "Avengers",
-            image: "https://i.postimg.cc/T3C92KTN/scale.jpg",
-            date: "2023-10-15",
-            description:
-                "Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-            category: "Cinema",
-            place: "Room D1",
-            capacity: 9000,
-            estimate: 9000,
-            price: 250,
-            __v: 0,
-        },
-    ],
-};
-
+let urlData = "https://aulamindhub.github.io/amazing-api/events.json";
 let urlDetails = new URL("http://localhost:5500/details.html");
-
 let inputSearch = document.querySelector(".form-control");
 let form = document.querySelector("form");
+let cardsHome = document.getElementById("cardsHome");
+let boxCheck = document.querySelector(".checkBox");
+let data = [];
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (inputSearch.value == "") {
         alert("What do you need?. Let us help you find it!");
+    } else {
+        filterEvents();
     }
 });
 
-let cardsHome = document.getElementById("cardsHome");
-cards(data.events, cardsHome);
+inputSearch.addEventListener("input", filterEvents);
 
-let boxCheck = document.querySelector(".checkBox");
-let arrayCategories = [];
-for (let event of data.events) {
-    if (!arrayCategories.includes(event.category))
-        arrayCategories.push(event.category);
-}
+fetch(urlData)
+    .then(response => response.json())
+    .then(fetchedData => {
+        data = fetchedData;
+        generateCategoryCheckboxes(data, boxCheck);
+        cards(data.events, cardsHome);
+    });
 
-for (let category of arrayCategories) {
-    let inputCheckbox = document.createElement("div");
-    inputCheckbox.innerHTML = `
-    <input class="form-check-input mt-0 inputCheckbox" id="${category}" type="checkbox" value="${category}" name="${category}" aria-label="${category}">
-    <label for="${category}" class="ms-2">${category}</label>`;
-    inputCheckbox.className = "input-group-text";
-    boxCheck.appendChild(inputCheckbox);
+function generateCategoryCheckboxes(data, boxCheck) {
+    let arrayCategories = [];
+    for (let event of data.events) {
+        if (!arrayCategories.includes(event.category)) {
+            arrayCategories.push(event.category);
+        }
+    }
+
+    for (let category of arrayCategories) {
+        let inputCheckbox = document.createElement("div");
+        inputCheckbox.innerHTML = `
+            <input class="form-check-input mt-0 inputCheckbox" id="${category}" type="checkbox" value="${category}" name="${category}" aria-label="${category}">
+            <label for="${category}" class="ms-2">${category}</label>`;
+        inputCheckbox.className = "input-group-text";
+        boxCheck.appendChild(inputCheckbox);
+    }
+
+    const checkboxes = document.querySelectorAll("input[type=checkbox]");
+    checkboxes.forEach(checkbox => checkbox.addEventListener("change", filterEvents));
 }
 
 function cards(array, cardsHome) {
     cardsHome.innerHTML = "";
-    for (let i = 0; i < array.length; i++) {
+    for (let event of array) {
+        let urlTemp = urlDetails + "?id=" + event._id;
         let card = document.createElement("div");
-        let urlTemp = urlDetails + "?id=" + array[i]._id;
         card.innerHTML = `
-        <div class="card me-3 my-1 h-100 cards-i" style="width: 18rem">
-            <img src="${array[i].image}" class="card-img-top" alt="${array[i].name}" style="height: 20vh; object-fit: cover;"/>
-            <div class="card-body text-center d-flex flex-column justify-content-around">
-                <h5 class="card-title">${array[i].name}</h5>
-                <p class="card-text">
-                    ${array[i].description}
-                </p>
-                <div class="d-flex justify-content-around align-items-baseline">
-                    <p>US ${array[i].price} USD</p>
-                    <a href="${urlTemp}" class="btn go-details">Details</a>                    
+            <div class="card me-3 my-1 h-100 cards-i" style="width: 18rem">
+                <img src="${event.image}" class="card-img-top" alt="${event.name}" style="height: 20vh; object-fit: cover;"/>
+                <div class="card-body text-center d-flex flex-column justify-content-around">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                    <div class="d-flex justify-content-around align-items-baseline">
+                        <p>US ${event.price} USD</p>
+                        <a href="${urlTemp}" class="btn go-details">Details</a>
+                    </div>
                 </div>
             </div>
-        </div>
         `;
         card.classList.add("my-3");
         cardsHome.appendChild(card);
     }
 }
 
-function filterEvents() {
-    const searchText = inputSearch.value.trim().toLowerCase();
-    const selectedCheckboxes = [...document.querySelectorAll("input[type=checkbox]:checked")].map(checkbox => checkbox.value);
-
-    let filteredEvents = data.events.filter(event =>
+function applyFilters(events, searchText, selectedCheckboxes) {
+    let filteredEvents = events.filter(event =>
         event.name.toLowerCase().includes(searchText) ||
         event.description.toLowerCase().includes(searchText)
     );
@@ -272,6 +88,12 @@ function filterEvents() {
     }
 }
 
+function filterEvents() {
+    const searchText = inputSearch.value.trim().toLowerCase();
+    const selectedCheckboxes = [...document.querySelectorAll("input[type=checkbox]:checked")].map(checkbox => checkbox.value);
+    applyFilters(data.events, searchText, selectedCheckboxes);
+}
+
 function showAlert(message) {
     cardsHome.innerHTML = "";
     const alert = document.createElement("div");
@@ -280,19 +102,3 @@ function showAlert(message) {
     alert.innerText = message;
     cardsHome.appendChild(alert);
 }
-
-inputSearch.addEventListener("input", () => {
-    if (inputSearch.value.trim() === "" && document.querySelectorAll("input[type=checkbox]:checked").length === 0) {
-        cards(data.events, cardsHome);
-    } else {
-        filterEvents();
-    }
-});
-
-boxCheck.addEventListener("change", () => {
-    if (inputSearch.value.trim() === "" && document.querySelectorAll("input[type=checkbox]:checked").length === 0) {
-        cards(data.events, cardsHome);
-    } else {
-        filterEvents();
-    }
-});
